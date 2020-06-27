30fef2c49edd5b4ad0e41df4b8d137c8e5ee34fe
Moohyun Song <crewn_adm@naver.com>
2020-06-27 21:05:18 +0900

    issue #6

--- calc/number.py
+++ calc/number.py
@@ -6,6 +6,10 @@ def application(environ, start_response):
     d = parse_qs(environ['QUERY_STRING'])
     a = d.get('a', [''])[0]
     b = d.get('b', [''])[0]
+    if '' in [a, b]:
+        f = open("/usr/local/swp1/result/result.txt", "w")
+        f.write("Please enter two numbers to calculate.")
+
     if '' not in [a, b]:
         a, b = int(a), int(b)
         number_sum = "%d + %d = %d" %(a,b,a+b)
