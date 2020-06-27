d4b00ed2577126303d134ac7e81a17d42f427b6a
Moohyun Song <crewn_adm@naver.com>
2020-06-27 21:30:30 +0900

    issue #7

--- calc/number.py
+++ calc/number.py
@@ -6,11 +6,8 @@ def application(environ, start_response):
     d = parse_qs(environ['QUERY_STRING'])
     a = d.get('a', [''])[0]
     b = d.get('b', [''])[0]
-    if '' in [a, b]:
-        f = open("/usr/local/swp1/result/result.txt", "w")
-        f.write("Please enter two numbers to calculate.")
 
-    if '' not in [a, b]:
+    try:
         a, b = int(a), int(b)
         number_sum = "%d + %d = %d" %(a,b,a+b)
         number_multi = "%d * %d = %d" %(a,b,a*b)
@@ -19,6 +16,9 @@ def application(environ, start_response):
         f.write(number_sum)
         f.write("\n")
         f.write(number_multi)
+    except:
+        f = open("/usr/local/swp1/result/result.txt", "w")
+        f.write("Please enter two numbers to calculate.")
 
     response_body = html
     start_response('200 OK', [
