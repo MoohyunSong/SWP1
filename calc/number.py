ce252fe6a93035b9b667c05c4736039b333e0bb9
Moohyun Song <crewn_adm@naver.com>
2020-06-27 20:11:27 +0900

    issue #4 WSGI 코드 버전 관리 시작

---
+++ calc/number.py
@@ -0,0 +1,28 @@
+from cgi import parse_qs
+from number_template import html
+import matplotlib.pyplot as plt
+
+def application(environ, start_response):
+    d = parse_qs(environ['QUERY_STRING'])
+    a = d.get('a', [''])[0]
+    b = d.get('b', [''])[0]
+    if '' not in [a, b]:
+        a, b = int(a), int(b)
+        number_sum = "%d + %d = %d" %(a,b,a+b)
+        number_multi = "%d * %d = %d" %(a,b,a*b)
+
+        f = open("/var/www/html/result/result.txt", "w")
+        f.write(number_sum)
+        f.write("\n")
+        f.write(number_multi)
+
+    response_body = html
+    start_response('200 OK', [
+        ('Content-Type', 'text/html'),
+        ('Content-Length', str(len(response_body)))
+    ])
+
+    return [response_body]
+
+
+
---
+++ calc/number_template.py
@@ -0,0 +1,15 @@
+html = b"""
+<html>
+    <body>
+        <form action="">
+            <input type="number" name="a">,
+            <input type="number" name="b">
+            <br><br>
+            <input type="submit">
+        <form>
+        <p>Please enter the numbers.</p>
+        <iframe src="/result/result.txt"> </iframe>
+    </body>
+</html>
+"""
+            
